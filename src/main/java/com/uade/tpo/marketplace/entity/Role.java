1881af6e20cbead4a1ac78a5a6902424c318761a
Ramiro Abadie <111799181+RamiroAbadie@users.noreply.github.com>
2025-04-07 18:06:03 -0300

    Delete import in Role
    Borre un import que no se usaba en Role

--- src/main/java/com/uade/tpo/marketplace/entity/Role.java
+++ src/main/java/com/uade/tpo/marketplace/entity/Role.java
@@ -1,7 +1,5 @@
 package com.uade.tpo.marketplace.entity;
 
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
