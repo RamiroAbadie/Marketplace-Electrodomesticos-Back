1c7ff9c53b6a2b42917279b667225d35cfe2a8c8
Patricio Assad <patricio.assad@gmail.com>
2025-04-18 19:32:11 -0300

    Se agregan clases para la autenticación

--- src/main/java/com/uade/tpo/marketplace/entity/dto/auth/RegisterRequest.java
+++ src/main/java/com/uade/tpo/marketplace/entity/dto/auth/RegisterRequest.java
@@ -1,21 +1,30 @@
-package com.uade.tpo.marketplace.entity.dto;
+package com.uade.tpo.marketplace.entity.dto.auth;
 
+import com.uade.tpo.marketplace.entity.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
 
     @NotBlank(message = "El nombre no puede estar vacío")
-    private String name;
+    private String firstname;
 
     @NotBlank(message = "El apellido no puede estar vacío")
-    private String surname;
+    private String lastname;
 
     @Email(message = "El email debe tener un formato válido")
     private String email;
 
     @NotBlank(message = "La contraseña no puede estar vacía")
     private String password;
+
+    private Role role;
 }
