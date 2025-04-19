a048db00f6bb6459279ddef11f6b2b1513b44721
Ramiro Abadie <111799181+RamiroAbadie@users.noreply.github.com>
2025-04-19 16:19:29 -0300

    Reapply "Merge remote-tracking branch 'origin/main'"
    This reverts commit 1f418324cf7fb42b2bb60bb6036ea85981c3fbf4.

--- src/main/java/com/uade/tpo/marketplace/entity/dto/RegisterRequest.java
+++ src/main/java/com/uade/tpo/marketplace/entity/dto/RegisterRequest.java
@@ -1,30 +1,21 @@
-package com.uade.tpo.marketplace.entity.dto.auth;
+package com.uade.tpo.marketplace.entity.dto;
 
-import com.uade.tpo.marketplace.entity.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class RegisterRequest {
 
     @NotBlank(message = "El nombre no puede estar vacío")
-    private String firstname;
+    private String name;
 
     @NotBlank(message = "El apellido no puede estar vacío")
-    private String lastname;
+    private String surname;
 
     @Email(message = "El email debe tener un formato válido")
     private String email;
 
     @NotBlank(message = "La contraseña no puede estar vacía")
     private String password;
-
-    private Role role;
 }
